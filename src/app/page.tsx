b16c29fe85eb2cd82db2e136a3f0cf4ab67959bc
Choi-jujuyeon <chlwndus1216@naver.com>
2023-09-05 00:30:25 +0900

    layout-with hardcoding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
 import { NextPage } from "next";
 const Home: NextPage = () => {
-    return <main className="bg-red-100">Home</main>;
+    return (
+        <main className="bg-slate-100 py-24 px-12 min-h-screen flex flex-col justify-center items-center">
+            {/* 상단 - 프로젝트 이름 */}
+            <div className="font-semibold ">REVIEW ENGLISH</div>
+
+            {/* 하단 - 버튼 3개(반복요소) */}
+            <ul className="mt-12 bg-slate-200 w-full grid grid-cols-4 gap-4">
+                <li className="bg-slate-300">
+                    <button>Day 1</button>
+                </li>
+                <li className="bg-slate-300">
+                    <button>Day 2</button>
+                </li>
+                <li className="bg-slate-300">
+                    <button>Day 3</button>
+                </li>
+            </ul>
+        </main>
+    );
 };
 export default Home;
