bc0bdfcfcc24175e543b49b144fe6ac80671b609
Choi-jujuyeon <chlwndus1216@naver.com>
2023-09-05 01:40:37 +0900

    link-go to mainpage

--- src/app/day/[id]/page.tsx
+++ src/app/day/[id]/page.tsx
@@ -1,7 +1,18 @@
 import { NextPage } from "next";
-
+import Link from "next/link";
 // 페이지 컴포넌트 == next-type인 NextPage로 타입 지정해주기
 const Day: NextPage = () => {
-    return <main>Day</main>;
+    return (
+        <main>
+            <div>
+                <Link href="/">
+                    <div>
+                        <button>Back</button>
+                    </div>
+                </Link>
+                <div>Day 1</div>
+            </div>
+        </main>
+    );
 };
 export default Day;
