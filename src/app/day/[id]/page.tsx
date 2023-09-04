38792bb6ac10cf5f389601ce8e051a452484dbe4
Choi-jujuyeon <chlwndus1216@naver.com>
2023-09-05 01:22:24 +0900

    dynamic-routing

---
+++ src/app/day/[id]/page.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+// 페이지 컴포넌트 == next-type인 NextPage로 타입 지정해주기
+const Day: NextPage = () => {
+    return <main>Day</main>;
+};
+export default Day;
--- src/components/DayCard.tsx
+++ src/components/DayCard.tsx
@@ -1,6 +1,6 @@
 // 컴포넌트 타입 FC 활용하기
 import { FC } from "react";
-
+import Link from "next/link";
 //props type을 적어줘야 한다 !
 // interface vs type
 type DayCardProps = {
@@ -13,9 +13,13 @@ type DayCardProps = {
 // 만들어준 props를 붙일 경우 == generic 형태로 붙여준다.
 const DayCard: FC<DayCardProps> = ({ index }) => {
     return (
-        <li className=" border-black border-2  font-medium rounded-lg shadow-md shadow-slate-400">
-            <button>Day {index + 1}</button>
-        </li>
+        <Link href={`/day/${index + 1}`}>
+            <li className=" border-black border-2  font-medium rounded-lg shadow-md shadow-slate-400">
+                {/* 각 버튼마다 링크 달아주기 */}
+                {/* 동적라우팅 사용할 경우 {`사용하기`} */}
+                <button>Day {index + 1}</button>
+            </li>
+        </Link>
     );
 };
 
