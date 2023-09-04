8991a574a1f71ae3cf5b9c046657aad1af2bc004
Choi-jujuyeon <chlwndus1216@naver.com>
2023-09-05 00:43:33 +0900

    button-styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,15 @@ const Home: NextPage = () => {
             <div className="font-semibold ">REVIEW ENGLISH</div>
 
             {/* 하단 - 버튼 3개(반복요소) */}
-            <ul className="mt-12 bg-slate-200 w-full grid grid-cols-4 gap-4">
-                <li className="bg-slate-300">
+            <ul className="mt-12  w-full grid grid-cols-4 gap-4 text-center">
+                {/* 테두리 색 시정해주기 - 색/ 두께 */}
+                <li className=" border-black border-2  font-medium rounded-lg shadow-md shadow-slate-400">
                     <button>Day 1</button>
                 </li>
-                <li className="bg-slate-300">
+                <li className=" border-black border-2  font-medium rounded-lg shadow-md shadow-slate-400">
                     <button>Day 2</button>
                 </li>
-                <li className="bg-slate-300">
+                <li className=" border-black border-2  font-medium rounded-lg shadow-md shadow-slate-400">
                     <button>Day 3</button>
                 </li>
             </ul>
