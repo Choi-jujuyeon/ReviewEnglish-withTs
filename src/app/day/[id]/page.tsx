cabb6ff0c895e72fb326b1ff21dbade5b93da4ea
Choi-jujuyeon <chlwndus1216@naver.com>
2023-09-07 16:05:40 +0900

    btn-style

--- src/app/day/[id]/page.tsx
+++ src/app/day/[id]/page.tsx
@@ -3,14 +3,17 @@ import Link from "next/link";
 // 페이지 컴포넌트 == next-type인 NextPage로 타입 지정해주기
 const Day: NextPage = () => {
     return (
-        <main>
-            <div>
+        <main className="bg-slate-100 min-h-screen flex flex-col justify-start items-center py-24 px-12">
+            <div className="relative  flex w-full justify-center">
                 <Link href="/">
-                    <div>
-                        <button>Back</button>
+                    {/* absolute : 절대값을 준다== relative랑 꼭 같이 사용해야 한다 ! */}
+                    <div className="absolute top-0 left-0">
+                        <button className="btn-style text-xs font-semibold">
+                            Back
+                        </button>
                     </div>
                 </Link>
-                <div>Day 1</div>
+                <div className="font-semibold mb-24 mt-2">Day 1</div>
             </div>
         </main>
     );
