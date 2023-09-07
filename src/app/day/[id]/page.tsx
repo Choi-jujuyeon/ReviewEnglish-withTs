9a17b7c736bfcb395f11850a49571157e6d66d34
Choi-jujuyeon <chlwndus1216@naver.com>
2023-09-07 16:26:57 +0900

    detail-page-styleComplete

--- src/app/day/[id]/page.tsx
+++ src/app/day/[id]/page.tsx
@@ -1,3 +1,6 @@
+import LeftArrow from "@/app/icons/LeftArrow";
+import RightArrow from "@/app/icons/RightArrow";
+import Speaker from "@/app/icons/Speaker";
 import { NextPage } from "next";
 import Link from "next/link";
 // 페이지 컴포넌트 == next-type인 NextPage로 타입 지정해주기
@@ -14,6 +17,32 @@ const Day: NextPage = () => {
                     </div>
                 </Link>
                 <div className="font-semibold mb-24 mt-2">Day 1</div>
+            </div>
+
+            <div className="bg-red-100 w-full">
+                <div className="bg-blue-100 h-60">
+                    <div className="border-black border-2 px-4 py-2">
+                        Working from home isn't for me. I always get distracted
+                    </div>
+                    <div className="mt-2">
+                        <button className="btn-style">
+                            <Speaker />
+                        </button>
+                    </div>
+                </div>
+                <div className="mt-4 flex justify-between">
+                    <button className="btn-style">
+                        <LeftArrow />
+                    </button>
+                    <button className="btn-style">
+                        {/* 줄바꿈이 안 일어날 경우에는 span태그 활용함 */}
+                        <span className="mr-1">KOR</span>/
+                        <span className="ml-1 font-semibold">ENG</span>
+                    </button>
+                    <button className="btn-style">
+                        <RightArrow />
+                    </button>
+                </div>
             </div>
         </main>
     );
