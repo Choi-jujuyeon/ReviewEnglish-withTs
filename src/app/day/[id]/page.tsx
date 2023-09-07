e8740bd4a57109167dbac8b05bbef7babdcc2b22
Choi-jujuyeon <chlwndus1216@naver.com>
2023-09-07 16:33:58 +0900

    useParams-Day{id}

--- src/app/day/[id]/page.tsx
+++ src/app/day/[id]/page.tsx
@@ -1,10 +1,19 @@
+"use client";
 import LeftArrow from "@/app/icons/LeftArrow";
 import RightArrow from "@/app/icons/RightArrow";
 import Speaker from "@/app/icons/Speaker";
 import { NextPage } from "next";
 import Link from "next/link";
+
+//동적 라우팅으로 구현한 다른 day 1~를 title에도 설정해주자!
+import { useParams } from "next/navigation";
+
 // 페이지 컴포넌트 == next-type인 NextPage로 타입 지정해주기
 const Day: NextPage = () => {
+    // 주소창의 값을 가져온다
+    // { } ~> 폴더명과 동일한 이름 사용학;
+    const { id } = useParams();
+
     return (
         <main className="bg-slate-100 min-h-screen flex flex-col justify-start items-center py-24 px-12">
             <div className="relative  flex w-full justify-center">
@@ -16,7 +25,7 @@ const Day: NextPage = () => {
                         </button>
                     </div>
                 </Link>
-                <div className="font-semibold mb-24 mt-2">Day 1</div>
+                <div className="font-semibold mb-24 mt-2">Day {id}</div>
             </div>
 
             <div className="bg-red-100 w-full">
